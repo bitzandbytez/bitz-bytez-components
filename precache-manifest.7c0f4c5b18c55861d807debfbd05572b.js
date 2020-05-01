866607f916425aaffbd915bad17c58277a435e4a
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-04-30 23:57:38 -0400

    Updates

---
+++ precache-manifest.7c0f4c5b18c55861d807debfbd05572b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06efc75d4256e0eb4f9363f16b4046eb",
+    "url": "/bitz-bytez-components/index.html"
+  },
+  {
+    "revision": "a8b4f73b1610a89197bc",
+    "url": "/bitz-bytez-components/static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "8e19c0b3cbdadc5203b2",
+    "url": "/bitz-bytez-components/static/js/2.0968486d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bitz-bytez-components/static/js/2.0968486d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b4f73b1610a89197bc",
+    "url": "/bitz-bytez-components/static/js/main.f507c9d7.chunk.js"
+  },
+  {
+    "revision": "f7dbdbd7da329eb6d066",
+    "url": "/bitz-bytez-components/static/js/runtime-main.e62e217e.js"
+  }
+]);
